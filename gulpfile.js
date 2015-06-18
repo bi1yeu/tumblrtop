f355441536554e2c517723407323ea28f549aea2
Matthew Bilyeu <bilyeu.matt@gmail.com>
2015-06-18 00:34:20 -0400

    Added 'clean' task to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,29 @@
 var gulp = require('gulp');
+var clean = require('gulp-clean');
 var exec = require('child_process').exec;
+
+var PORT_NUM = 3000;
+
 var bowerDir = './bower_components/';
 var publicDir = './dist/';
-var jsDir = 'js/';
+var jsDir = publicDir + 'js/';
+
+gulp.task('clean', function() {
+  gulp.src(jsDir + '*', {read: false})
+    .pipe(clean());
+});
 
 gulp.task('build', function() {
-   gulp.src(bowerDir + '**/*.js')
-   .pipe(gulp.dest(publicDir + jsDir));
+  gulp.src(bowerDir + '**/*.js')
+    .pipe(gulp.dest(jsDir));
 });
 
 gulp.task('serve', function() {
-  exec('node server.js', function (err, stdout, stderr) {
+  exec('node server.js ' + PORT_NUM, function (err, stdout, stderr) {
     console.log(stdout);
     console.log(stderr);
   })
 });
 
-gulp.task('default', ['build', 'serve'], function() {
+gulp.task('default', ['clean', 'build', 'serve'], function() {
 });
--- server.js
+++ server.js
@@ -1,3 +1,11 @@
 var connect = require('connect');
 var serveStatic = require('serve-static');
-connect().use(serveStatic(__dirname + '/dist/')).listen(3000);
+
+var PORT_NUM = process.argv[2];
+
+if (PORT_NUM) {
+  connect().use(serveStatic(__dirname + '/dist/')).listen(PORT_NUM);
+} else {
+  console.log('No port specified; exiting');
+  process.exit(1);
+}
