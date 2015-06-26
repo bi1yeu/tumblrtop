bb05a0418c793e33b43d5c1d0bb77d4cca8a6e59
Matthew Bilyeu <bilyeu.matt@gmail.com>
2015-06-25 22:12:16 -0400

    Cleaning up api key management

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,8 @@ var gulp = require('gulp'),
     coffee = require('gulp-coffee'),
     concat = require('gulp-concat'),
     ngAnnotate = require('gulp-ng-annotate'),
-    templateCache = require('gulp-angular-templatecache');
+    templateCache = require('gulp-angular-templatecache')
+    shell = require('gulp-shell');
 
 var PORT_NUM = 3000;
 
@@ -16,7 +17,11 @@ var bowerDir = './bower_components/',
     htmlFiles = srcDir + 'views/**/*.html',
     jsDir = distDir + 'js/';
 
-gulp.task('clean', function(cb) {
+gulp.task('unset-key', shell.task([
+   "sed -i '' \"s/API_KEY = '.*'/API_KEY = '<consumer-key>'/g\" */**/tumblrService.coffee"
+]));
+
+gulp.task('clean', ['unset-key'], function(cb) {
   del(jsDir + '*', cb);
 });
 
@@ -30,7 +35,13 @@ gulp.task('html', function () {
         .pipe(gulp.dest(jsDir));
 });
 
-gulp.task('build', ['clean', 'html'], function() {
+// This gets the consumer key env var and just writes it to the source
+// A corresponding pre-commit hook should remove it
+gulp.task('set-key', ['clean'], shell.task([
+  "sed -i '' \"s/API_KEY = '<consumer-key>'/API_KEY = '$TUMBLR_CONSUMER_KEY'/g\" */**/tumblrService.coffee"
+]));
+
+gulp.task('build', ['clean', 'set-key', 'html'], function() {
   gulp.src(bowerDir + '**/*.min.js')
     .pipe(gulp.dest(jsDir));
   gulp.src(coffeeFiles)
