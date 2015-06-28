4e5722177ab0e04520731163c795fd0de598f510
Matthew Bilyeu <bilyeu.matt@gmail.com>
2015-06-28 15:39:07 -0400

    Improve build process; add watcher

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 var gulp = require('gulp'),
     del = require('del'),
     exec = require('child_process').exec,
-    gutil = require('gulp-util'),
+    plumber = require('gulp-plumber'),
     coffee = require('gulp-coffee'),
     concat = require('gulp-concat'),
     ngAnnotate = require('gulp-ng-annotate'),
@@ -13,12 +13,12 @@ var PORT_NUM = 3000;
 var bowerDir = './bower_components/',
     srcDir = './src/',
     distDir = './dist/',
-    coffeeFiles = srcDir + '**/*.coffee',
+    coffeeFiles = [srcDir + '**/*.coffee', srcDir + '*.coffee'],
     htmlFiles = srcDir + 'views/**/*.html',
     jsDir = distDir + 'js/';
 
 gulp.task('unset-key', shell.task([
-   "sed -i '' \"s/API_KEY = '.*'/API_KEY = '<consumer-key>'/g\" */**/tumblrService.coffee"
+  "sed -i '' \"s/API_KEY = '.*'/API_KEY = '<consumer-key>'/g\" */**/tumblrService.coffee"
 ]));
 
 gulp.task('clean', ['unset-key'], function(cb) {
@@ -37,18 +37,26 @@ gulp.task('html', function () {
 
 // This gets the consumer key env var and just writes it to the source
 // A corresponding pre-commit hook should remove it
-gulp.task('set-key', ['clean'], shell.task([
+gulp.task('set-key', shell.task([
   "sed -i '' \"s/API_KEY = '<consumer-key>'/API_KEY = '$TUMBLR_CONSUMER_KEY'/g\" */**/tumblrService.coffee"
 ]));
 
-gulp.task('build', ['clean', 'set-key', 'html'], function() {
+gulp.task('coffee', function() {
+  gulp.src(coffeeFiles)
+      .pipe(plumber())
+      .pipe(coffee({bare: true}))
+      .pipe(concat('app.js'))
+      .pipe(ngAnnotate())
+      .pipe(gulp.dest(jsDir));
+})
+
+gulp.task('build', ['clean', 'set-key', 'coffee', 'html'], function() {
   gulp.src(bowerDir + '**/*.min.js')
     .pipe(gulp.dest(jsDir));
-  gulp.src(coffeeFiles)
-    .pipe(coffee({bare: true}).on('error', gutil.log))
-    .pipe(ngAnnotate())
-    .pipe(concat('app.js'))
-    .pipe(gulp.dest(jsDir));
+});
+
+gulp.task('watch', function () {
+  watcher = gulp.watch([coffeeFiles, htmlFiles], ['coffee', 'html']);
 });
 
 gulp.task('serve', function() {
